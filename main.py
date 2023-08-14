5c4b1ac24e5ef5d03f02c608697b3a862b175593
Tomsh666 <tomshinskiy02@gmail.com>
2023-08-14 22:26:08 +0300

    make notes problems with pic

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
-import json
+import os
 import base64
-from datetime import datetime
+from urllib.parse import urlparse
 def vkData():
     print()
-    # access token
+    # access token vk
     access_token = "fd218039fd218039fd2180391dfe34becdffd21fd21803999f0bb0cea30602a831c43c0"
     group_id = -221937101     #для пользователя положительное, для группы отрицательное
-    count = 5
+    count = 1
 
     # Ссылка на метод VK API для получения постов со стены группы
     vk_api_url = f"https://api.vk.com/method/wall.get?owner_id={group_id}&count={count}&access_token={access_token}&v=5.131"
@@ -36,44 +36,55 @@ def vkData():
                         # print("Фотография:", photo_url) # url фотографии
                         wp_dict["photos"].append(photo_url)
             # print(wp_dict) # вывод словарь для json
-            json_data = json.dumps(wp_dict)
             # print(json_data) # выводит готовый json
-            SendWp(json_data)
+            SendWp(wp_dict)
             wp_dict["text"]=""
             wp_dict["photos"]=[]
     else:
         print("Ошибка при получении данных.")
 
-def SendWp(json_data):
+#загрузка изображений в папку(не факт что нужно)
+def PicDown(image_url):
+    save_directory = "D:\MAMP\htdocs\wp_test\wp-content\images"  # дириктория где храняться картинки
+    response = requests.get(image_url)
+
+    if response.status_code == 200:
+        parsed_url = urlparse(image_url)
+        image_filename = os.path.basename(parsed_url.path)
+        image_path = os.path.join(save_directory, image_filename)
+
+        with open(image_path, "wb") as f:
+            f.write(response.content)
+        return image_filename
+    else:
+        print("Ошибка при скачивании изображения.")
+
+
+
+def SendWp(data):
     # url сайта на wp
-    wp_url = "http://localhost/wp_test/wp-json/wp/v2"
+    wp_url = "http://localhost/wp_test/wp-json/wp/v2/"
     # в wp-config.php define( 'WP_ENVIRONMENT_TYPE', 'local' );
     # creds юзер + пароль приложения
-    user = "admin"
-    password = "I08Y aqua 6zcT wt84 4r8Q AXtA"
-    # Получаем текущую дату и время
-    current_datetime = datetime.now()
-    # Форматируем дату и время в нужный формат
-    formatted_datetime = current_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+    user = 'admin'
+    password = '2KgQ W4cd_VhM7 Epk9 vTS9 iBBK'
     creds = user + ':' + password
     token = base64.b64encode(creds.encode())
-    header = {"Authorization":"Basic" + token.decode('utf-8')}
-    post ={
-        'date': formatted_datetime,
-        'title': 'hello api',
-        'content': 'asasaasasasaasaaassaaa',
+    header = {"Authorization": "Basic " + token.decode('utf-8')}
+    post = {
+        'content': '<!-- wp:paragraph -->' + data["text"] + '<!-- /wp:paragraph -->',
         'status': 'publish'
     }
 
+    for pic in data["photos"]:
+        #post['content']+='<!-- wp:image --><figure class="wp-block-imag"><img src="' + pic + '" alt="picture"></figure><!-- /wp:image -->'
+        file_name = PicDown(pic)
 
-    response = requests.post(wp_url + '/posts', headers=header,json=post)  # отправка в wp
+    response = requests.post(wp_url + 'posts', headers=header,json=post)  # отправка в wp
     if response.status_code == 201:
         print('Запись успешно создана.')
     else:
         print('Произошла ошибка:', response.text)
-
-
-
 
 
 
